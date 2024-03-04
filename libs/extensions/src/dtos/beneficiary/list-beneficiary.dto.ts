bac3914a5c617cd1d3466753e6ecd25a623ab0f8
Raghav Kattel <arg.ahv7@gmail.com>
2024-03-04 11:47:04 +0545

    Comment out unused code in ListBeneficiaryDto

--- libs/extensions/src/dtos/beneficiary/list-beneficiary.dto.ts
+++ libs/extensions/src/dtos/beneficiary/list-beneficiary.dto.ts
@@ -1,12 +1,12 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { PaginationDto } from '@rumsan/extensions/dtos';
-import { IsIn, IsOptional, IsString } from 'class-validator';
+// import { PaginationDto } from '@rumsan/extensions/dtos';
+import { IsOptional, IsString } from 'class-validator';
 
-export class ListBeneficiaryDto extends PaginationDto {
-  @IsIn(['createdAt', 'updatedAt', 'fullName', 'gender'])
-  override sort = 'createdAt';
+export class ListBeneficiaryDto {
+  // @IsIn(['createdAt', 'updatedAt', 'fullName', 'gender'])
+  // override sort = 'createdAt';
 
-  override order: 'asc' | 'desc' = 'desc';
+  // override order: 'asc' | 'desc' = 'desc';
 
   @ApiPropertyOptional({ example: 'd8f61ebb-ae83-4a8b-8f36-ed756aa27d12' })
   @IsString()
