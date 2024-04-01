b5f5e46942998ebb6276b1500a98f788c5765253
myanzik <myanz.470shrestha@gmail.com>
2024-04-01 21:13:14 +0545

    action: fix vendor assign to project

--- libs/extensions/src/dtos/vendor/vendor.add.project.dto.ts
+++ libs/extensions/src/dtos/vendor/vendor.add.project.dto.ts
@@ -8,5 +8,5 @@ export class VendorAddToProjectDto {
 
   @ApiProperty({ example: 'd8f61ebb-ae83-4a8b-8f36-ed756aa27d12' })
   @IsString()
-  vendorUuid: string | undefined;
+  vendorId: string | undefined;
 }
