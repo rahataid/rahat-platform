0d039bc721fdb721cd96085a94566c2cb977ff0a
manjik-rumsan <manjik.shrestha@rumsan.com>
2024-04-15 12:39:09 +0545

    refactor project action

---
+++ apps/rahat/src/projects/actions/aa.action.ts
@@ -0,0 +1,24 @@
+
+import {
+    AAJobs,
+    MS_ACTIONS
+} from '@rahataid/sdk';
+
+export const aaActions = {
+    /***********************
+     * Development Only
+    *************************/
+    [MS_ACTIONS.AAPROJECT.SCHEDULE.DEV_ONLY]: (uuid, payload, sendCommand) =>
+        sendCommand({ cmd: AAJobs.SCHEDULE.DEV_ONLY, uuid }, payload),
+    /************************/
+
+    [MS_ACTIONS.AAPROJECT.SCHEDULE.ADD]: (uuid, payload, sendCommand) =>
+        sendCommand({ cmd: AAJobs.SCHEDULE.ADD, uuid }, payload),
+
+    [MS_ACTIONS.AAPROJECT.SCHEDULE.REMOVE]: (uuid, payload, sendCommand) =>
+        sendCommand({ cmd: AAJobs.SCHEDULE.REMOVE, uuid }, payload),
+
+    [MS_ACTIONS.AAPROJECT.SCHEDULE.GET_ALL]: (uuid, payload, sendCommand) =>
+        sendCommand({ cmd: AAJobs.SCHEDULE.GET_ALL, uuid }, {}),
+
+};
---
+++ apps/rahat/src/projects/actions/index.ts
@@ -0,0 +1,4 @@
+export { aaActions } from './aa.action';
+export { beneficiaryActions, vendorActions } from './common.action';
+export { elActions } from './el.action';
+
