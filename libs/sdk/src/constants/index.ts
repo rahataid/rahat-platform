be2488fa24a38dc942a286a940352c7920b61cf6
avash <avash.ghimire@rumsan.net>
2024-05-10 16:29:42 +0545

    update: added trigger comms aa action

--- libs/sdk/src/constants/index.ts
+++ libs/sdk/src/constants/index.ts
@@ -97,7 +97,6 @@ export const MS_ACTIONS = {
     COMMUNICATION: {
       ADD: 'aaProject.activities.communication.add',
       TRIGGER: 'aaProject.activities.communication.trigger',
-
     },
     ACTIVITY_CATEGORIES: {
       GET_ALL: 'aaProject.activityCategories.getAll',
