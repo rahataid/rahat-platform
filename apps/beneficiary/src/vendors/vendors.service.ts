f614682c153bf5b78d883f87bea20457999f98ab
anupama-rumsan <anupama@rumsan.net>
2024-06-13 11:17:45 +0545

    user email must be unique error added

--- apps/beneficiary/src/vendors/vendors.service.ts
+++ apps/beneficiary/src/vendors/vendors.service.ts
@@ -31,6 +31,10 @@ export class VendorsService {
     if (!role) throw new Error('Role not found');
     // Add to User table
     const { service, ...rest } = dto;
+    const userData = await this.prisma.user.findFirst({
+      where: { email: dto.email }
+    })
+    if (userData) throw new Error("Email must be unique");
     const user = await this.prisma.user.create({ data: rest });
     // Add to UserRole table
     const userRolePayload = { userId: user.id, roleId: role.id };
@@ -233,6 +237,10 @@ export class VendorsService {
 
   async updateVendor(dto) {
     const { uuid, ...rest } = dto;
+    const userData = await this.prisma.user.findFirst({
+      where: { email: dto.email }
+    })
+    if (userData) throw new Error("Email must be unique");
     const result = await this.usersService.update(uuid, rest);
     return result;
 
