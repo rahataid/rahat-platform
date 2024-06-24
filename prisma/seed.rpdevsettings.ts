81e5a6595a23b5e95e88d8621ccca3db6459489b
raghav-rumsan <raghav.kattel@rumsan.net>
2024-06-24 14:08:13 +0545

    chore: Add RP_DEV seed settings script

---
+++ prisma/seed.rpdevsettings.ts
@@ -0,0 +1,97 @@
+import { Prisma, PrismaClient, Setting, SettingDataType } from '@prisma/client';
+import * as fs from 'fs/promises';
+
+const prisma = new PrismaClient();
+
+const rootPath = process.argv[2]
+const rootEnv = `${rootPath}/.env`
+const gnacheAccounts = `${rootPath}/accounts.json`
+
+async function main() {
+  const accounts = await getAccounts()
+  const adminAccounts = accounts?.adminAccounts as string[]
+  const pvtKey = accounts?.privateKey as string[]
+
+  await modifyRootEnv(pvtKey[0])
+
+  /*********** cva dev settings  **************** */
+  const rpDevSettings: Setting = {
+    value: {
+      privateKey: pvtKey[0],
+      adminAccounts: adminAccounts
+    } as Prisma.JsonValue,
+    isPrivate: true,
+    isReadOnly: true,
+    name: 'CVA_DEV',
+    requiredFields: ['privateKey', 'adminAccounts'],
+    dataType: SettingDataType.OBJECT,
+  }
+
+  await prisma.setting.upsert({
+    where: {
+      name: 'RP_DEV'
+    },
+    // @ts-ignore
+    create: rpDevSettings,
+    // @ts-ignore
+    update: rpDevSettings
+  });
+  /*********** cva dev settings end  **************** */
+
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
+
+
+async function getAccounts() {
+  try {
+
+    const accounts = await fs.readFile(gnacheAccounts, 'utf-8');
+    const ganacheData = JSON.parse(accounts)
+
+    const allAccounts = ganacheData.addresses;
+    const allKeys = ganacheData.private_keys
+
+    const adminAccounts = Object.values(allAccounts).slice(0, 2);
+    const privateKey = Object.values(allKeys).slice(0, 1);
+
+    return {
+      adminAccounts,
+      privateKey
+    }
+  } catch (error) {
+    console.error('Error reading accounts file:', error);
+  }
+}
+
+
+async function modifyRootEnv(key: string) {
+  try {
+    let data = await fs.readFile(rootEnv, 'utf-8');
+    const lines = data.split('\n') as string[];
+
+    const newLines = lines.map(line => {
+      if (line.startsWith('PRIVATE_KEY')) {
+        return `PRIVATE_KEY=${key}`;
+      }
+      return line;
+    });
+
+    const newData = newLines.join('\n');
+
+    await fs.writeFile(rootEnv, newData, 'utf-8');
+
+    console.log(rootEnv)
+    console.log('File updated.');
+  } catch (error) {
+    console.error('Error modifying dev-tools .env file:', error);
+  }
+}
+
