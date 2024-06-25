8534778ddd9b49e1b07c5bfe916df9de5d744c69
raghav-rumsan <raghav.kattel@rumsan.net>
2024-06-25 19:46:43 +0545

    chore: Add script to create project navigation
    ```

---
+++ tools/scripts/add-project-nav.ts
@@ -0,0 +1,54 @@
+import { PrismaClient } from '@prisma/client';
+import { PrismaService, } from '@rumsan/prisma';
+import { SettingsService } from '@rumsan/settings';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const prisma = new PrismaService();
+const settings = new SettingsService(prisma);
+const prismaClient = new PrismaClient()
+
+const navJSON = {
+  data: [],
+
+  subData: [
+    {
+      title: 'Treasury',
+      path: '/treasury',
+    },
+  ],
+};
+
+const main = async () => {
+  // const settingsData: Setting = {
+  //   value: navJSON as Prisma.JsonValue,
+  //   isPrivate: true,
+  //   isReadOnly: true,
+  //   name: 'NAV_SETTINGS',
+
+  //   requiredFields: [],
+  //   dataType: 'OBJECT',
+  // };
+
+  await prismaClient.setting.create({
+    data: {
+      value: navJSON as object,
+      isPrivate: false,
+      isReadOnly: true,
+      name: 'NAV_SETTINGS',
+      dataType: 'OBJECT',
+
+      requiredFields: ['data', 'subData'],
+    }
+  });
+};
+
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
