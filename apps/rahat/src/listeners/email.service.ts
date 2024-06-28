d7dc24c2a9e16c56206e5f6e10a4a1f16c48756f
Santosh Shrestha <santosh@rumsan.com>
2024-06-28 15:29:36 +0545

    fixed SMTP break

--- apps/rahat/src/listeners/email.service.ts
+++ apps/rahat/src/listeners/email.service.ts
@@ -13,16 +13,20 @@ export class EmailService {
   initialize() {
     if (!this.transporter) {
       console.log('Initializing email service');
-      this.transporter = nodemailer.createTransport({
-        pool: true,
-        host: SettingsService.get('SMTP.HOST'),
-        port: SettingsService.get('SMTP.PORT'),
-        secure: SettingsService.get('SMTP.SECURE'),
-        auth: {
-          user: SettingsService.get('SMTP.USERNAME'),
-          pass: SettingsService.get('SMTP.PASSWORD'),
-        },
-      });
+      try {
+        this.transporter = nodemailer.createTransport({
+          pool: true,
+          host: SettingsService.get('SMTP.HOST'),
+          port: SettingsService.get('SMTP.PORT'),
+          secure: SettingsService.get('SMTP.SECURE'),
+          auth: {
+            user: SettingsService.get('SMTP.USERNAME'),
+            pass: SettingsService.get('SMTP.PASSWORD'),
+          },
+        });
+      } catch {
+        throw new Error("SMTP settings not available")
+      }
     }
   }
 
@@ -32,21 +36,20 @@ export class EmailService {
     text: string,
     html?: string
   ): Promise<void> {
-    this.initialize();
-    const mailOptions = {
-      from: SettingsService.get('SMTP.USERNAME'), // Replace with your email
-      to: to,
-      subject: subject,
-      text: text,
-      html: html,
-    };
-
     try {
+      this.initialize();
+      const mailOptions = {
+        from: SettingsService.get('SMTP.USERNAME'), // Replace with your email
+        to: to,
+        subject: subject,
+        text: text,
+        html: html,
+      };
       const info = await this.transporter.sendMail(mailOptions);
       console.log('Email sent:', info.messageId);
     } catch (error) {
-      console.error('Error sending email:', error);
-      throw error; // Or handle it as per your application needs
+      //console.error('Error sending email:', error);
+      //throw error; // Or handle it as per your application needs
     }
   }
 }
