8c55df61719dffb6d5b428c9fbefc271c37ac0db
anupama-rumsan <anupama@rumsan.net>
2024-07-11 13:46:33 +0545

    unused code cleanups

--- apps/contracts/scripts/generateStandardJson.js
+++ apps/contracts/scripts/generateStandardJson.js
@@ -2,16 +2,16 @@ const fs = require('fs');
 
 //Read build files
 const readAllDbgFiles = () => {
-    const dbgFiles = fs.readdirSync(`${__dirname}/../build/artifacts/src/contracts`);
+    const dbgFiles = fs.readdirSync(`${__dirname}/../artifacts/src/contracts`);
     const buildFilePaths = [];
     dbgFiles.forEach((file) => {
-        const files = fs.readdirSync(`${__dirname}/../build/artifacts/src/contracts/${file}`);
+        const files = fs.readdirSync(`${__dirname}/../artifacts/src/contracts/${file}`);
         files.forEach((f) => {
             if (f.includes('.dbg.json')) {
                 //read the content of the file
                 //remove .dbg.json from file name string
                 const fileName = f.replace('.dbg.json', '');
-                const content = JSON.parse(fs.readFileSync(`${__dirname}/../build/artifacts/src/contracts/${file}/${f}`, 'utf8'));
+                const content = JSON.parse(fs.readFileSync(`${__dirname}/../artifacts/src/contracts/${file}/${f}`, 'utf8'));
                 //only get the last id from this string format '../../../build-info/8b726545c8bf4ae06e6409c02d82af7e.json
                 buildFilePaths.push({ contract: fileName, buildInfoId: content.buildInfo.split('/').at(-1).replace('.json', '') });
             }
@@ -28,7 +28,7 @@ const main = async () => {
         if (!fs.existsSync(`${__dirname}/standard-json`)) {
             fs.mkdirSync(`${__dirname}/standard-json`);
         }
-        const content = JSON.parse(fs.readFileSync(`${__dirname}/../build/artifacts/build-info/${file.buildInfoId}.json`, 'utf8'));
+        const content = JSON.parse(fs.readFileSync(`${__dirname}/../artifacts/build-info/${file.buildInfoId}.json`, 'utf8'));
         fs.writeFileSync(`${__dirname}/standard-json/${file.contract}.json`, JSON.stringify(content.input, null, 2));
         console.log(`Standard json file generated for ${file.contract}`);
     });
