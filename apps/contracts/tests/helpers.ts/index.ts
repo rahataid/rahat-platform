bccca26f0c03982c81a671c5fbd3897173e96fa8
anupama-rumsan <anupama@rumsan.net>
2024-07-11 13:50:50 +0545

    utility functions added in contracts library

--- apps/contracts/tests/helpers.ts/index.ts
+++
@@ -1,5 +0,0 @@
-import { ethers } from "ethers";
-
-export const getFunctionId = (signature: string) => {
-    return ethers.FunctionFragment.from(signature).selector;
-}
--- libs/contract/src/lib/contract.spec.ts
+++
@@ -1,7 +0,0 @@
-import { contract } from './contract';
-
-describe('contract', () => {
-  it('should work', () => {
-    expect(contract()).toEqual('contract');
-  });
-});
