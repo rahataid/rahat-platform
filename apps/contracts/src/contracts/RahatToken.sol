866f8719b90b92c2eeb15574ec4eae2cdfbe561e
anupama-rumsan <anupama@rumsan.net>
2024-07-11 13:38:04 +0545

    contracts library restructured

--- apps/contracts/src/contracts/RahatToken.sol
+++ apps/contracts/src/contracts/RahatToken.sol
@@ -3,45 +3,44 @@ pragma solidity 0.8.20;
 
 //ERC20 Tokens
 import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol';
-
-import '../interfaces/IRahatToken.sol';
-import '../libraries/AbstractOwner.sol';
+import '@rumsan/contracts/interfaces/IRahatToken.sol';
+import '@rumsan/contracts/libraries/AbstractOwner.sol';
 import '@openzeppelin/contracts/access/manager/AccessManaged.sol';
 
 contract RahatToken is
-  AbstractOwner,
-  ERC20,
-  ERC20Burnable,
-  IRahatToken,
-  AccessManaged
+    AbstractOwner,
+    ERC20,
+    ERC20Burnable,
+    IRahatToken,
+    AccessManaged
 {
-  uint8 private decimalPoints;
-  string public description;
+    uint8 private decimalPoints;
+    string public description;
 
-  constructor(
-    string memory _name,
-    string memory _symbol,
-    string memory _description,
-    uint8 _decimals,
-    uint256 _initialSupply,
-    address _to,
-    address _manager
-  ) ERC20(_name, _symbol) AccessManaged(_manager) {
-    decimalPoints = _decimals;
-    description = _description;
-    _mint(_to, _initialSupply);
-  }
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        string memory _description,
+        uint8 _decimals,
+        uint256 _initialSupply,
+        address _to,
+        address _manager
+    ) ERC20(_name, _symbol) AccessManaged(_manager) {
+        decimalPoints = _decimals;
+        description = _description;
+        _mint(_to, _initialSupply);
+    }
 
-  ///@dev returns the decimals of the tokens
-  function decimals() public view override returns (uint8) {
-    return decimalPoints;
-  }
+    ///@dev returns the decimals of the tokens
+    function decimals() public view override returns (uint8) {
+        return decimalPoints;
+    }
 
-  ///@dev Mint x amount of ERC20 token to given address
-  ///@param _address Address to which ERC20 token will be minted
-  ///@param _amount Amount of token to be minted
-  function mint(address _address, uint256 _amount) public returns (uint256) {
-    _mint(_address, _amount);
-    return _amount;
-  }
+    ///@dev Mint x amount of ERC20 token to given address
+    ///@param _address Address to which ERC20 token will be minted
+    ///@param _amount Amount of token to be minted
+    function mint(address _address, uint256 _amount) public returns (uint256) {
+        _mint(_address, _amount);
+        return _amount;
+    }
 }
--- apps/contracts/src/contracts/RahatTreasury.sol
+++ apps/contracts/src/contracts/RahatTreasury.sol
@@ -5,109 +5,114 @@ import '@openzeppelin/contracts/utils/structs/EnumerableSet.sol';
 import '@openzeppelin/contracts/utils/introspection/ERC165.sol';
 import '@openzeppelin/contracts/access/manager/AccessManaged.sol';
 import './RahatToken.sol';
-import '../libraries/AbstractTokenActions.sol';
-import '../interfaces/IRahatProject.sol';
-import '../interfaces/IRahatTreasury.sol';
+import '@rumsan/contracts/interfaces/IRahatTreasury.sol';
+import '@rumsan/contracts/interfaces/IRahatProject.sol';
+import '@rumsan/contracts/libraries/AbstractTokenActions.sol';
 
 /// @title Treasury contract to manage Rahat tokens and generate tokens
 /// @author Rumsan Associates
 /// @notice You can use this contract to manage Rahat tokens
 contract RahatTreasury is AbstractTokenActions, ERC165, AccessManaged {
-  using EnumerableSet for EnumerableSet.AddressSet;
+    using EnumerableSet for EnumerableSet.AddressSet;
 
-  event TokenCreated(address indexed tokenAddress);
-  event TokenMintedAndApproved(
-    address indexed tokenAddress,
-    address indexed approveAddress,
-    uint256 amount
-  );
-  event TokenMintedAndSent(
-    address indexed tokenAddress,
-    address indexed receiverAddress,
-    uint256 amount
-  );
-
-  /// @notice All the supply is allocated to this contract
-  /// @dev deploys AidToken and Rahat contract by sending supply to this contract
-
-  bytes4 public constant IID_RAHAT_TREASURY = type(IRahatTreasury).interfaceId;
-  EnumerableSet.AddressSet private tokens;
-
-  constructor(address _manager) AccessManaged(_manager) {}
-
-  function getTokens() public view returns (address[] memory) {
-    address[] memory _tokens = new address[](tokens.length());
-    for (uint256 i = 0; i < tokens.length(); i++) {
-      _tokens[i] = tokens.at(i);
-    }
-    return _tokens;
-  }
-
-  //#region Token function
-  function createToken(
-    string memory _name,
-    string memory _symbol,
-    string memory _description,
-    uint8 decimals,
-    uint256 _initialSupply,
-    address _to,
-    address _manager
-  ) public returns (address) {
-    RahatToken _token = new RahatToken(
-      _name,
-      _symbol,
-      _description,
-      decimals,
-      _initialSupply,
-      _to,
-      _manager
+    event TokenCreated(address indexed tokenAddress);
+    event TokenMintedAndApproved(
+        address indexed tokenAddress,
+        address indexed approveAddress,
+        uint256 amount
     );
-    address _tokenAddress = address(_token);
-    tokens.add(_tokenAddress);
-    emit TokenCreated(_tokenAddress);
-    return _tokenAddress;
-  }
+    event TokenMintedAndSent(
+        address indexed tokenAddress,
+        address indexed receiverAddress,
+        uint256 amount
+    );
 
-  function mintToken(address _token, uint256 _amount) public {
-    RahatToken(_token).mint(address(this), _amount);
-  }
+    /// @notice All the supply is allocated to this contract
+    /// @dev deploys AidToken and Rahat contract by sending supply to this contract
 
-  function mintTokenAndApprove(
-    address _token,
-    address _approveAddress,
-    uint256 _amount
-  ) public {
-    require(_token != address(0), 'token address cannot be zero');
-    require(_approveAddress != address(0), 'approve address cannot be zero');
-    require(_amount > 0, 'amount cannot be zero');
+    bytes4 public constant IID_RAHAT_TREASURY =
+        type(IRahatTreasury).interfaceId;
+    EnumerableSet.AddressSet private tokens;
 
-    RahatToken token = RahatToken(_token);
-    token.mint(address(this), _amount);
-    token.approve(_approveAddress, _amount);
-    emit TokenMintedAndApproved(_token, _approveAddress, _amount);
-  }
+    constructor(address _manager) AccessManaged(_manager) {}
 
-  function mintTokenAndSend(
-    address _token,
-    address _receiver,
-    uint256 _amount
-  ) public {
-    require(_token != address(0), 'token address cannot be zero');
-    require(_receiver != address(0), 'approve address cannot be zero');
-    require(_amount > 0, 'amount cannot be zero');
+    function getTokens() public view returns (address[] memory) {
+        address[] memory _tokens = new address[](tokens.length());
+        for (uint256 i = 0; i < tokens.length(); i++) {
+            _tokens[i] = tokens.at(i);
+        }
+        return _tokens;
+    }
 
-    RahatToken token = RahatToken(_token);
-    token.mint(address(this), _amount);
-    token.transfer(_receiver, _amount);
-    emit TokenMintedAndApproved(_token, _receiver, _amount);
-  }
+    //#region Token function
+    function createToken(
+        string memory _name,
+        string memory _symbol,
+        string memory _description,
+        uint8 decimals,
+        uint256 _initialSupply,
+        address _to,
+        address _manager
+    ) public returns (address) {
+        RahatToken _token = new RahatToken(
+            _name,
+            _symbol,
+            _description,
+            decimals,
+            _initialSupply,
+            _to,
+            _manager
+        );
+        address _tokenAddress = address(_token);
+        tokens.add(_tokenAddress);
+        emit TokenCreated(_tokenAddress);
+        return _tokenAddress;
+    }
 
-  function supportsInterface(
-    bytes4 interfaceId
-  ) public view virtual override returns (bool) {
-    return
-      interfaceId == IID_RAHAT_TREASURY || super.supportsInterface(interfaceId);
-  }
+    function mintToken(address _token, uint256 _amount) public {
+        RahatToken(_token).mint(address(this), _amount);
+    }
 
-  //#endregion
+    function mintTokenAndApprove(
+        address _token,
+        address _approveAddress,
+        uint256 _amount
+    ) public {
+        require(_token != address(0), 'token address cannot be zero');
+        require(
+            _approveAddress != address(0),
+            'approve address cannot be zero'
+        );
+        require(_amount > 0, 'amount cannot be zero');
+
+        RahatToken token = RahatToken(_token);
+        token.mint(address(this), _amount);
+        token.approve(_approveAddress, _amount);
+        emit TokenMintedAndApproved(_token, _approveAddress, _amount);
+    }
+
+    function mintTokenAndSend(
+        address _token,
+        address _receiver,
+        uint256 _amount
+    ) public {
+        require(_token != address(0), 'token address cannot be zero');
+        require(_receiver != address(0), 'approve address cannot be zero');
+        require(_amount > 0, 'amount cannot be zero');
+
+        RahatToken token = RahatToken(_token);
+        token.mint(address(this), _amount);
+        token.transfer(_receiver, _amount);
+        emit TokenMintedAndApproved(_token, _receiver, _amount);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override returns (bool) {
+        return
+            interfaceId == IID_RAHAT_TREASURY ||
+            super.supportsInterface(interfaceId);
+    }
+
+    //#endregion
 }
