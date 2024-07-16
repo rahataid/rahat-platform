18c932f3598a62d6c52d8bd9ee88f1500b536a95
anupama-rumsan <anupama@rumsan.net>
2024-07-16 15:41:29 +0545

    new actions added for redemptions in rp

--- libs/sdk/src/constants/index.ts
+++ libs/sdk/src/constants/index.ts
@@ -200,6 +200,7 @@ export const MS_ACTIONS = {
     GET_ALL_COMMUNICATION_LOGS: 'cvaProject.campaign.communication_logs',
     GET_ALL_COMMUNICATION_STATS: 'cvaProject.campaign.communication_stats',
 
+
   },
   RPPROJECT: {
     CREATE_DISBURSEMENT: 'rpProject.disbursement.create',
@@ -220,5 +221,9 @@ export const MS_ACTIONS = {
     TRIGGER_CAMPAIGN: 'rpProject.campaign.trigger',
     GET_ALL_COMMUNICATION_LOGS: 'rpProject.campaign.communication_logs',
     GET_ALL_COMMUNICATION_STATS: 'rpProject.campaign.communication_stats',
+    REQUEST_REDEMPTION: 'rpProject.requestRedemption',
+    UPDATE_REDEMPTION: 'rpProject.updateRedemption',
+    LIST_REDEMPTION: 'rpProject.listRedemption',
+    GET_VENDOR_REDEMPTION: 'rpProject.vendorRedemption',
   }
 };
