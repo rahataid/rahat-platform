667653800783de406fd792b4a1d24d1c7a7bb5ff
lishu-rumsan <lishu.maharjan@rumsan.net>
2024-08-22 10:31:58 +0545

    feat: jest-setup-grievance-test

---
+++ apps/rahat-e2e/src/rahat/grievances/testFixtureData.ts
@@ -0,0 +1,40 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import { faker } from '@faker-js/faker';
+import { GrievanceStatus, GrievanceType } from '@prisma/client';
+import { randomUUID } from 'crypto';
+
+export const grievanceData = {
+    projectId: process.env.PROJECT_ID,
+    reporterContact: faker.phone.number(),
+    title: faker.internet.domainWord(),
+    description: faker.lorem.lines(),
+    type: faker.helpers.enumValue(GrievanceType),
+    status: faker.helpers.enumValue(GrievanceStatus),
+    reportedBy: faker.person.fullName()
+};
+
+export const errorGrievance = {
+    reporterContact: faker.phone.number(),
+    title: faker.internet.domainWord(),
+    description: faker.lorem.lines(),
+    type: faker.helpers.enumValue(GrievanceType),
+    status: faker.helpers.enumValue(GrievanceStatus),
+    reportedBy: faker.person.fullName()
+};
+
+export const grievanceQuery = {
+    sort: 'createdAt',
+    order: 'asc',
+    page: 1,
+    perPage: 10,
+    projectId: process.env.PROJECT_ID
+}
+
+export const errorGrievanceQuery = {
+    sort: 'createdAt',
+    order: 'asc',
+    page: 1,
+    perPage: 10,
+    projectId: randomUUID()
+}
