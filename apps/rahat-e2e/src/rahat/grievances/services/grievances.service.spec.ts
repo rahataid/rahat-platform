72a349274cf6c1ba801a1205cce450a08979f516
lishu-rumsan <lishu.maharjan@rumsan.net>
2024-08-23 16:59:31 +0545

    feat: grievances-e2e-service-test

--- apps/rahat-e2e/src/rahat/grievances/services/grievances.service.spec.ts
+++ apps/rahat-e2e/src/rahat/grievances/services/grievances.service.spec.ts
@@ -1,15 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GrievanceService } from '../../../../../rahat/src/grievance/grievance.service';
-import { PrismaService } from '@rumsan/prisma';
+import { PaginatorTypes, PrismaService, paginator } from "@rumsan/prisma";
 import { GrievanceStatus } from '@prisma/client';
-import { createGrievanceDTO, expectedGrievance, updatedGrievance, userId } from './testFixtureData';
-import { ChangeGrievanceStatusDTO, CreateGrievanceDTO } from '@rahataid/extensions';
+import { createGrievanceDTO, deletedGrievance, expectedGrievance, grievanceQuery, grievancesList, updatedGrievance, userId } from './testFixtureData';
+import { ChangeGrievanceStatusDTO } from '@rahataid/extensions';
 
 describe('GrievanceService', () => {
   let service: GrievanceService;
   let prisma: PrismaService;
+  let paginate: jest.Mock;
 
   beforeEach(async () => {
+    paginate = jest.fn().mockResolvedValue({
+      data: grievancesList,
+      page: 1,
+      perPage: 20,
+      total: grievancesList.length,
+    });
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         GrievanceService,
@@ -20,8 +28,13 @@ describe('GrievanceService', () => {
               create: jest.fn(),
               update: jest.fn(),
               findUnique: jest.fn(),
+              delete: jest.fn()
             },
           },
+        },
+        {
+          provide: 'paginate',
+          useValue: paginate
         },
       ],
     }).compile();
@@ -56,16 +69,74 @@ describe('GrievanceService', () => {
     expect(result).toEqual(expectedGrievance);
   }); 
 
-  it('should update the grievance status', async () => {
+  // it('should return list of paginated grievances as per query', async () => {
+  //   const result = await service.findAll(grievanceQuery);
+  //   console.log(result, 'result');
+  //   expect(paginate).toHaveBeenCalledWith(
+  //     prisma.grievance,
+  //     {
+  //       include: {
+  //         project: { select: { name: true, uuid: true } },
+  //         reporterUser: { select: { name: true, uuid: true, id: true } },
+  //       },
+  //       where: {
+  //         deletedAt: null,
+  //         projectId: {
+  //           equals: grievanceQuery.projectId,
+  //         },
+  //       },
+  //     },
+  //     {
+  //       page: grievanceQuery.page,
+  //       perPage: grievanceQuery.perPage,
+  //     },
+  //   );
+  //   expect(result).toEqual({
+  //     data: grievancesList,
+  //     page: 1,
+  //     perPage: 20,
+  //     total: grievancesList.length,
+  //   });
+  // });
+
+  it('should retrieve a grievance with associated project and reporterUser', async () => {
+    const uuid = process.env.GRIEVANCE_UUID;
+    jest.spyOn(prisma.grievance, 'findUnique').mockResolvedValue(grievancesList[0]);
+    const result = await service.getGrievance(uuid);
+    expect(prisma.grievance.findUnique).toHaveBeenCalledWith({
+      where: { uuid },
+      include: {
+        project: {
+          select: { name: true, uuid: true },
+        },
+        reporterUser: {
+          select: { name: true, uuid: true, id: true },
+        },
+      },
+    });
+    expect(result).toEqual(grievancesList[0]);
+  });
+
+  it('should update the grievance status using uuid', async () => {
     const uuid  = process.env.GRIEVANCE_UUID;
     const data = { status: 'NEW' } as ChangeGrievanceStatusDTO;
     jest.spyOn(prisma.grievance, 'update').mockResolvedValue(updatedGrievance);
     const result = await service.changeStatus(uuid, data);
-    console.log(result, 'result');
     expect(prisma.grievance.update).toHaveBeenCalledWith({
       where: {uuid},
       data: {status: data.status}
     });
     expect(result).toEqual(updatedGrievance);
+  });
+
+  it('should soft delete the grievance using uuid', async () => {
+    const uuid = 'test-uuid';
+    jest.spyOn(prisma.grievance, 'update').mockResolvedValue(deletedGrievance);
+    const result = await service.softDelete(uuid);
+    expect(prisma.grievance.update).toHaveBeenCalledWith({
+      where: { uuid },
+      data: { deletedAt: expect.any(Date) },
+    });
+    expect(result).toEqual(deletedGrievance);
   });
 });
--- apps/rahat-e2e/src/rahat/grievances/services/testFixtureData.ts
+++ apps/rahat-e2e/src/rahat/grievances/services/testFixtureData.ts
@@ -1,15 +1,14 @@
-import { CreateGrievanceDTO } from "@rahataid/extensions";
+import { faker } from "@faker-js/faker";
+import { CreateGrievanceDTO, ListGrievanceDTO } from "@rahataid/extensions";
 import { GrievanceStatus, GrievanceType } from "@rahataid/sdk";
 
 export const createGrievanceDTO: CreateGrievanceDTO = {
-    // id: '1',
-    // uuid: "uuid",
     projectId: process.env.PROJECT_ID,
-    title: 'Test Grievance',
-    description: 'This is a test grievance',
-    reporterContact: "9843848488",
+    title: faker.internet.domainWord(),
+    description: faker.lorem.lines(),
+    reporterContact: faker.phone.number(),
     type: GrievanceType.TECHNICAL,
-    reportedBy: 'Hello Test',
+    reportedBy: faker.person.fullName(),
     status: GrievanceStatus.NEW
 };
 
@@ -25,65 +24,90 @@ export const expectedGrievance = {
 export const updatedGrievance = {
   id: 1,
   uuid: process.env.GRIEVANCE_UUID,
-  reportedBy: 'Hello Test',
+  reportedBy: faker.person.fullName(),
   reporterUserId: userId,
-  reporterContact: "9843848488",
-  title: 'Test Grievance',
+  reporterContact: faker.phone.number(),
+  title: faker.internet.domainWord(),
   type: GrievanceType.TECHNICAL,
   projectId: process.env.PROJECT_ID,
-  description: 'This is a test grievance',
+  description: faker.lorem.lines(),
   status: GrievanceStatus.UNDER_REVIEW,
   createdAt: new Date(),
   updatedAt: new Date(),
   deletedAt: null
 };
 
+export const deletedGrievance = {
+  id: 1,
+  uuid: process.env.GRIEVANCE_UUID,
+  reportedBy: faker.person.fullName(),
+  reporterUserId: userId,
+  reporterContact: faker.phone.number(),
+  title: faker.internet.domainWord(),
+  type: GrievanceType.TECHNICAL,
+  projectId: process.env.PROJECT_ID,
+  description: faker.lorem.lines(),
+  status: GrievanceStatus.UNDER_REVIEW,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  deletedAt: new Date()
+};
+
+export const grievanceQuery: ListGrievanceDTO = {
+  sort: 'createAt',
+  order: 'asc',
+  page: 1,
+  perPage: 20,
+  projectId: process.env.PROJECT_ID
+};
+
 export const grievancesList = [
-    {
-      "id": 1,
-      "uuid": "7789a21f-b959-4486-97eb-f802d1b2b321",
-      "reportedBy": "Sarvesh Karki",
-      "reporterUserId": 1,
-      "reporterContact": "9841234567",
-      "title": "Grievance title",
-      "type": "TECHNICAL",
-      "projectId": "04cb0d02-e003-45ab-82a7-5b3b04889339",
-      "description": "Grievance description",
-      "status": "NEW",
-      "createdAt": "2024-08-21T15:54:32.779Z",
-      "updatedAt": "2024-08-21T15:54:32.779Z",
-      "deletedAt": null,
-      "project": {
-        "name": "stickybeak",
-        "uuid": "04cb0d02-e003-45ab-82a7-5b3b04889339"
-      },
-      "reporterUser": {
-        "name": "Anupama Koirala",
-        "uuid": "8e8f409e-b607-4ad1-b6cd-cc1e4f5a08c4",
-        "id": 1
-      }
+  {
+    id: 1,
+    uuid: process.env.GRIEVANCE_UUID,
+    reportedBy: faker.person.fullName(),
+    reporterUserId: 1,
+    reporterContact: faker.phone.number(),
+    title: faker.internet.domainWord(),
+    type: GrievanceType.TECHNICAL,
+    projectId: process.env.PROJECT_ID,
+    description: faker.lorem.lines(),
+    status: GrievanceStatus.NEW,
+    createdAt: new Date("2024-08-21T15:54:32.779Z"),
+    updatedAt: new Date("2024-08-21T15:54:32.779Z"),
+    deletedAt: null,
+    project: {
+      name: faker.internet.domainWord(),
+      uuid: process.env.PROJECT_ID
     },
-    {
-      "id": 2,
-      "uuid": "0ac168bb-c411-4cd2-994d-199522023de8",
-      "reportedBy": "Ram Prasad",
-      "reporterUserId": 1,
-      "reporterContact": "9841234567",
-      "title": "Jest Test",
-      "type": "TECHNICAL",
-      "projectId": "04cb0d02-e003-45ab-82a7-5b3b04889339",
-      "description": "Hello, this is grievance test case",
-      "status": "NEW",
-      "createdAt": "2024-08-21T15:55:25.616Z",
-      "updatedAt": "2024-08-21T15:55:25.616Z",
-      "deletedAt": null,
-      "project": {
-        "name": "stickybeak",
-        "uuid": "04cb0d02-e003-45ab-82a7-5b3b04889339"
-      },
-      "reporterUser": {
-        "name": "Anupama Koirala",
-        "uuid": "8e8f409e-b607-4ad1-b6cd-cc1e4f5a08c4",
-        "id": 1
-      }
-}];
+    reporterUser: {
+      name: process.env.NAME,
+      uuid: process.env.UUID,
+      id: 1
+    }
+  },
+  {
+    id: 2,
+    uuid: process.env.GRIEVANCE_UUID,
+    reportedBy: faker.person.fullName(),
+    reporterUserId: 1,
+    reporterContact: faker.phone.number(),
+    title: faker.internet.domainWord(),
+    type: GrievanceType.TECHNICAL,
+    projectId: process.env.PROJECT_ID,
+    description: faker.lorem.lines(),
+    status: GrievanceStatus.NEW,
+    createdAt: new Date("2024-08-21T15:54:32.779Z"),
+    updatedAt: new Date("2024-08-21T15:54:32.779Z"),
+    deletedAt: null,
+    project: {
+      name: faker.internet.domainWord(),
+      uuid: process.env.PROJECT_ID
+    },
+    reporterUser: {
+      name: process.env.NAME,
+      uuid: process.env.UUID,
+      id: 1
+    }
+  }
+];
