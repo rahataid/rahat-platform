24465cd156d052ee05796ed01e8ac96011feb569
Your Name <your.email@example.com>
2024-09-12 11:45:44 +0545

    stellar-platform-init

--- libs/sdk/src/project/project.events.ts
+++ libs/sdk/src/project/project.events.ts
@@ -58,7 +58,7 @@ export const JOBS = {
     GET_OFFLINE_VENDORS: "rahat.jobs.get_offline_vendors",
     GET_OFFLINE_SINGLE_VENDOR: "rahat.jobs.get_offline_single_vendor",
     SAVE_SYNCED_BENEFICIARIES_TO_VENDOR: "rahat.jobs.save_synced_beneficiaries_to_vendor",
-
+    UPDATE_OFFLINE_BENEFICIARY: 'rahat.jobs.beneficiary.update_offline',
   },
   BENEFICIARY: {
     ADD_GROUP: 'rahat.jobs.beneficiary.addGroup',
