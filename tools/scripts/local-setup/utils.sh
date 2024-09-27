689045841806002b40e3911d1641d55061c95d63
anupama-rumsan <anupama@rumsan.net>
2024-09-27 14:30:13 +0545

    bootstrap updated for kenya

--- tools/scripts/local-setup/utils.sh
+++ tools/scripts/local-setup/utils.sh
@@ -30,6 +30,7 @@ migrate_seed() {
     pnpm seed:aadevsettings $current_dir
     pnpm seed:cvadevsettings $current_dir
     pnpm seed:rpdevsettings $current_dir
+    pnpm seed:kenyadevsettings $current_dir
     pnpm seed:chainsettings
 }
 
