491f37f028845dc8c29149cda4d3530da6332ac9
Binod Rumsan <binod.chaudhary@rumsan.com>
2024-10-29 12:44:48 +0545

    update kobo field mapping action

--- apps/rahat/src/utils/fieldMappings.ts
+++ apps/rahat/src/utils/fieldMappings.ts
@@ -1,10 +1,10 @@
 export const KOBO_FIELD_MAPPINGS = {
-    Beneficiary_Name: 'name',
-    Beneficiary_Mobile_Number: 'phone',
-    Beneficiary_Age: 'age',
-    Email_address: 'email',
-    Data_type: 'type',
-    Beneficiary_Gender: 'gender',
-    Lead_Interest: 'leadInterests',
-    Beneficiary_Address: 'coordinates',
-}
+  Beneficiary_Name: 'name',
+  Beneficiary_Phone_Number: 'phone',
+  Beneficiary_Age: 'age',
+  Email_address: 'email',
+  Data_type: 'type',
+  Beneficiary_Gender: 'gender',
+  Lead_Interest: 'leadInterests',
+  Beneficiary_Address: 'coordinates',
+};
