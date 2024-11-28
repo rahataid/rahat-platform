1f00dd36f83f55a06d82f99ff4a2ddc6433c822a
raghav-rumsan <raghav.kattel@rumsan.net>
2024-11-28 14:24:55 +0545

    Update Kotani Pay service configuration and API integration

--- apps/rahat/src/offramp/offrampProviders/kotaniPay.service.ts
+++ apps/rahat/src/offramp/offrampProviders/kotaniPay.service.ts
@@ -6,115 +6,119 @@ import axios, { AxiosInstance } from 'axios';
 import { OfframpService } from './offrampService.interface'; // Adjust the import path as needed
 
 interface OfframpProviderConfig {
-    baseUrl: string;
-    apiKey: string;
+  baseUrl: string;
+  apiKey: string;
 }
 
 type TCreate = {
-    chain: string;
-    token: string;
-    amount: number;
-    senderAddress: string;
+  chain: string;
+  token: string;
+  amount: number;
+  senderAddress: string;
 }
 
 type TCheck = {
-    referenceId: string;
+  referenceId: string;
 }
 
 
 @Injectable()
 export class KotaniPayService implements OfframpService<TCreate, KotaniPayExecutionData, TCheck> {
-    constructor(private readonly prisma: PrismaService) { }
+  constructor(private readonly prisma: PrismaService) { }
 
-    private async getProviderConfig(uuid: string): Promise<OfframpProviderConfig> {
-        const offrampProvider = await this.prisma.offrampProvider.findUnique({
-            where: { uuid }
-        });
+  private async getProviderConfig(uuid: string): Promise<OfframpProviderConfig> {
+    const offrampProvider = await this.prisma.offrampProvider.findUnique({
+      where: { uuid }
+    });
 
-        if (!offrampProvider) {
-            throw new Error('Provider not found');
-        }
-
-        return offrampProvider.config as unknown as OfframpProviderConfig;
+    if (!offrampProvider) {
+      throw new Error('Provider not found');
     }
 
-    private async getKotaniPayAxiosClient(uuid: string): Promise<AxiosInstance> {
-        const { baseUrl, apiKey } = await this.getProviderConfig(uuid);
+    return offrampProvider.config as unknown as OfframpProviderConfig;
+  }
 
-        return axios.create({
-            baseURL: baseUrl,
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${apiKey}`
-            }
-        });
-    }
+  private async getKotaniPayAxiosClient(uuid: string): Promise<AxiosInstance> {
+    const { baseUrl, apiKey } = await this.getProviderConfig(uuid);
 
-    async createCustomerMobileMoneyWallet(data: ProviderActionDto) {
-        const client = await this.getKotaniPayAxiosClient(data.uuid);
-        const response = await client.post('/customer/mobile-money', {
-            country_code: data.payload.country_code,
-            phone_number: data.payload.phone_number,
-            network: data.payload.network,
-            account_name: data.payload.account_name
-        });
+    return axios.create({
+      baseURL: baseUrl,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`
+      }
+    });
+  }
 
-        return { data: response.data };
-    }
+  async createCustomerMobileMoneyWallet(data: ProviderActionDto) {
+    const client = await this.getKotaniPayAxiosClient(data.uuid);
+    const response = await client.post('/customer/mobile-money', {
+      country_code: data.payload.country_code,
+      phone_number: data.payload.phone_number,
+      network: data.payload.network,
+      account_name: data.payload.account_name
+    });
 
-    async listCustomerMobileMoneyWallet(data: ProviderActionDto) {
-        const client = await this.getKotaniPayAxiosClient(data.uuid);
-        const response = await client.get('/customer/mobile-money');
+    return { data: response.data };
+  }
 
-        return { data: response.data };
-    }
+  async listCustomerMobileMoneyWallet(data: ProviderActionDto) {
+    const client = await this.getKotaniPayAxiosClient(data.uuid);
+    const response = await client.get('/customer/mobile-money');
 
-    async createFiatWallet(data: ProviderActionDto) {
-        const client = await this.getKotaniPayAxiosClient(data.uuid);
-        const response = await client.post('/wallet/fiat', {
-            currency: data.payload.currency,
-            name: data.payload.name,
-        })
-        console.log({ response });
-        return { data: response.data };
-    }
+    return { data: response.data };
+  }
 
-    async getFiatWallet(data: ProviderActionDto) {
-        console.log({ data });
-        const client = await this.getKotaniPayAxiosClient(data.uuid);
-        const response = await client.get('/wallet/fiat');
-        console.log({ response });
-        return { data: response.data.data };
-    }
+  async createFiatWallet(data: ProviderActionDto) {
+    const client = await this.getKotaniPayAxiosClient(data.uuid);
+    const response = await client.post('/wallet/fiat', {
+      currency: data.payload.currency,
+      name: data.payload.name,
+    })
+    console.log({ response });
+    return { data: response.data };
+  }
 
-    async createOfframpRequest(providerUuid: string, data: TCreate): Promise<any> {
-        // Implementation goes here
-        const client = await this.getKotaniPayAxiosClient(providerUuid);
-        const response = await client.post('/offramp/crypto-to-fiat/mobile-money/request', {
-            chain: data.chain,
-            token: data.token,
-            senders_amount: data.amount,
-            senders_address: data.senderAddress
-        })
-        return { data: response.data };
-    }
+  async getFiatWallet(data: ProviderActionDto) {
+    console.log({ data });
+    const client = await this.getKotaniPayAxiosClient(data.uuid);
+    const response = await client.get('/wallet/fiat');
+    console.log({ response });
+    return { data: response.data.data };
+  }
 
-    async executeOfframpRequest(providerUuid: string, data: KotaniPayExecutionData): Promise<any> {
-        const client = await this.getKotaniPayAxiosClient(providerUuid);
-        const response = await client.post('/offramp/crypto-to-fiat/mobile-money', data)
-        return { data: response.data };
-    }
+  async createOfframpRequest(providerUuid: string, data: TCreate): Promise<any> {
+    // Implementation goes here
+    const client = await this.getKotaniPayAxiosClient(providerUuid);
+    const response = await client.post('/offramp/crypto-to-fiat/mobile-money/request', {
+      chain: data.chain,
+      token: data.token,
+      senders_amount: data.amount,
+      senders_address: data.senderAddress
+    })
+    return { data: response.data };
+  }
 
-    async checkOfframpStatus(data: any): Promise<any> {
-        // Implementation goes here
-        throw new Error('Method not implemented.');
-    }
+  async executeOfframpRequest(providerUuid: string, data: KotaniPayExecutionData): Promise<any> {
+    const client = await this.getKotaniPayAxiosClient(providerUuid);
+    const response = await client.post('/offramp/crypto-to-fiat/mobile-money', data)
+    return { data: response.data };
+  }
 
-    kotaniPayActions = {
-        'create-customer-mobile-wallet': this.createCustomerMobileMoneyWallet.bind(this),
-        'list-customer-mobile-wallet': this.listCustomerMobileMoneyWallet.bind(this),
-        'create-fiat-wallet': this.createFiatWallet.bind(this),
-        'get-fiat-wallet': this.getFiatWallet.bind(this),
-        // Add more Kotani Pay actions here
-    };
+  async checkOfframpStatus(data: any): Promise<any> {
+    console.log('data', data)
+    // Implementation goes here
+    const client = await this.getKotaniPayAxiosClient(data.uuid);
+    const response = await client.get(`/offramp/${data.payload.requestUuid}`);
+    return { data: response.data };
+  }
+
+  kotaniPayActions = {
+    'create-customer-mobile-wallet': this.createCustomerMobileMoneyWallet.bind(this),
+    'list-customer-mobile-wallet': this.listCustomerMobileMoneyWallet.bind(this),
+    'create-fiat-wallet': this.createFiatWallet.bind(this),
+    'get-fiat-wallet': this.getFiatWallet.bind(this),
+    'check-offramp-status': this.checkOfframpStatus.bind(this),
+    // Add more Kotani Pay actions here
+  };
 }
