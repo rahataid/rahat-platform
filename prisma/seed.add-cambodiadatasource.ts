1107e6e14f794c9b3adae2ba660ced6f363010a0
karun-rumsan <gyawalikarrun@gmail.com>
2025-02-21 17:48:22 +0545

    change in  cambodia db source

--- prisma/seed.add-cambodiadatasource.ts
+++ prisma/seed.add-cambodiadatasource.ts
@@ -14,7 +14,6 @@ const rl = readline.createInterface({
 const projectStats = (uuid: string, url: string) => {
   return {
     dataSources: {
-
       source3: {
         type: 'url',
         args: {
