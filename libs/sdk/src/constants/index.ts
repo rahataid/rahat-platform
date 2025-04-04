a6a83aaae420e733a4d0fe1473c7e98473ae60cb
Dipesh Kumar Sah <dipesh@rumsan.net>
2025-04-04 14:23:05 +0545

    chore: update bruno for rahat-triggers

--- libs/sdk/src/constants/index.ts
+++ libs/sdk/src/constants/index.ts
@@ -13,7 +13,7 @@ export const BQUEUE = {
   RAHAT_PROJECT: 'RAHAT.PROJECT',
   RAHAT_BENEFICIARY: 'RAHAT.BENEFICIARY',
   HOST: 'RAHAT.HOST',
-  META_TXN: 'RAHAT.META_TXN'
+  META_TXN: 'RAHAT.META_TXN',
 };
 
 export const UserRoles = {
@@ -369,6 +369,7 @@ export const MS_ACTIONS = {
     ADD: 'ms.triggers.add',
     REMOVE: 'ms.triggers.remove',
     ACTIVATE: 'ms.triggers.activate',
+    UPDATE: 'ms.triggers.update',
     GET_BY_LOCATION: 'ms.triggers.getByLocation',
   },
   MS_PHASES: {
