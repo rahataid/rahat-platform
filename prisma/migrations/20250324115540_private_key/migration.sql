514ff3079b53665d88361fc3ed4e90d3e73b6abd
myanzik <myanz.470shrestha@gmail.com>
2025-04-08 11:06:44 +0545

    removed migrations

--- prisma/migrations/20250324115540_private_key/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `privateKey` to the `tbl_users_wallets` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "tbl_users_wallets" ADD COLUMN     "privateKey" TEXT NOT NULL;
