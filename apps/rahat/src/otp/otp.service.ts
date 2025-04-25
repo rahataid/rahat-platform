821c4a08f7001da18f9a470b031fb01224068970
Sushant Tripathu <sushant@RS-L-25-02-Macair.local>
2025-04-25 19:09:07 +0545

    added env to settings

--- apps/rahat/src/otp/otp.service.ts
+++ apps/rahat/src/otp/otp.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreateClaimDto } from '@rahataid/extensions';
+import { SettingsService } from '@rumsan/extensions/settings';
+import { PrismaService } from '@rumsan/prisma';
 import prabhu from './sms/prabhu';
 
 @Injectable()
 export class OtpService {
+
+    constructor(
+        protected prisma: PrismaService,
+    ) { }
 
     async addOtpToClaim(data: CreateClaimDto) {
         const otp = await this.getOtp();
@@ -11,7 +17,11 @@ export class OtpService {
             otp,
             data.amount)
         const sms = await this.loadSmsModule();
-        await sms(data.phoneNumber, message);
+        await sms(data.phoneNumber, message, {
+            appId: this.getFromSettings('prabhu', 'appId'),
+            token: this.getFromSettings('prabhu', 'token'),
+            url: this.getFromSettings('prabhu', 'url')
+        });
         return { otp };
     }
 
@@ -23,7 +33,7 @@ export class OtpService {
         const smsModules = {
             prabhu,
         };
-        const serviceName: string = process.env.SMS_SERVICE || 'prabhu';
+        const serviceName: string = await this.getFromSettings('prabhu', 'provider') || 'prabhu';
 
         const module = smsModules[serviceName];
 
@@ -31,7 +41,7 @@ export class OtpService {
     };
 
     private async createMessage(otp: string | number, amount: string | number) {
-        let message: string = process.env.OTP_MESSAGE || 'Hi, your OTP is ${otp} and the amount is ${amount}';
+        let message: string = await this.getFromSettings('prabhu', 'message') || 'Hi, your OTP is ${otp} and the amount is ${amount}';
 
         if (message.includes('${amount}')) {
             message = message.replace('${amount}', amount.toString());
@@ -41,6 +51,13 @@ export class OtpService {
         }
 
         return message;
+    }
+
+    async getFromSettings(provider: string, key: string) {
+        const settings = new SettingsService(this.prisma);
+        const smsSettings: any = await settings.getByName('SMS_SETTINGS')
+        const result = smsSettings.value.find(item => item.provider === "prabhu");
+        return result[key];
     }
 }
 
--- apps/rahat/src/otp/sms/prabhu.ts
+++ apps/rahat/src/otp/sms/prabhu.ts
@@ -1,14 +1,12 @@
 import axios from 'axios';
 
-const url = process.env.PRABHU_URL || '';
-const token = process.env.PRABHU_TOKEN || '';
 
-export default async (phone: string, message: string) => {
+export default async (phone: string, message: string, extras: any) => {
     if (!phone) throw new Error('No recipient was specified');
     if (!message) throw new Error('No message was specified');
 
     const data = {
-        transport: process.env.PRABHU_APP_ID,
+        transport: await extras.appId,
         message: {
             content: message,
             meta: {
@@ -24,9 +22,10 @@ export default async (phone: string, message: string) => {
         }
     };
 
-    const response = await axios.post(url, data, {
+    console.log(await extras.url)
+    const response = await axios.post(await extras.url, data, {
         headers: {
-            "app-id": token,
+            "app-id": await extras.token,
             'Content-Type': 'application/json',
         },
     });
