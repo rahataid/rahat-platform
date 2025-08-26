e5cd515a638d0f06f031bc379f5628c1c6e5b9ec
karun-rumsan <gyawalikarrun@gmail.com>
2025-08-26 14:24:35 +0545

    email-template for the notificaiton service

--- apps/rahat/src/processors/rahat.processor.ts
+++ apps/rahat/src/processors/rahat.processor.ts
@@ -9,9 +9,7 @@ import { EmailService } from '../listeners/email.service';
 @Processor(BQUEUE.RAHAT)
 export class RahatProcessor {
   private readonly logger = new Logger(RahatProcessor.name);
-  constructor(
-    private emailService: EmailService
-  ) { }
+  constructor(private emailService: EmailService) { }
 
   @Process(APP_JOBS.EMAIL)
   async sendEmail(job: Job<any>) {
@@ -25,18 +23,55 @@ export class RahatProcessor {
   }
 
   @Process(APP_JOBS.NOTIFY)
-  async sendNotifyEmail(job: Job<{ usersEmail: string[], subject: string, text: string }>) {
+  async sendNotifyEmail(
+    job: Job<{ usersEmail: string[]; subject: string; text: string }>
+  ) {
     const data = job.data;
     const { usersEmail, subject, text } = data;
     await this.emailService.sendEmail(
       usersEmail,
       subject,
       `
-      Hi,
+      <div style="font-family: Arial, sans-serif; padding: 20px;">
+        <div style="max-width: 600px; margin: auto; background: #ffffff; border-radius: 12px;
+                    padding: 24px; text-align: center; border: 1px solid #111111;">
 
-      ${text}
+          <!-- Logo -->
+          <div style="margin-bottom: 20px;">
+            <img src='https://assets.rumsan.net/rumsan-group/rahat-logo-standard.png' width="150" title="Rahat" alt="Rahat";>
+          </div>
+
+          <!-- Inner Card -->
+          <div style="background-color: #f3f4f6; padding: 24px; border-radius: 16px; margin-bottom: 24px;">
+
+            <!-- Title -->
+            <h2 style="color: #111827; font-size: 20px; font-weight: 600; margin-bottom: 16px;">
+              ${subject}
+            </h2>
+
+            <!-- Content -->
+            <p style="color: #374151; font-size: 14px; line-height: 1.6; margin-bottom: 20px; text-align: center;">
+              Hi, <br><br>
+              ${text}
+            </p>
+
+            <!-- Divider -->
+            <hr style="border: none; border-top: 1px solid #e5e7eb; margin: 20px 0;" />
+
+            <!-- Footer -->
+            <p style="font-size: 12px; color: #6b7280; margin: 0;">
+              This is an automated notification from Rahat
+            </p>
+          </div>
+
+          <!-- Bottom Note -->
+          <p style="font-size: 12px; color: #9ca3af; margin: 0;">
+            © ${new Date().getFullYear()}, Rahat. All rights reserved.
+          </p>
+
+        </div>
+      </div>
       `
     );
   }
-
 }
