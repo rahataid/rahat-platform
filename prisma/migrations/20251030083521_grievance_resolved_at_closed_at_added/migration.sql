05fedabbdf727f0371e0c7f6aa0fb1069e5e9d65
prithvi-rumsan <prithvi.maharjan@rumsan.net>
2025-10-31 12:15:20 +0545

    Migrations update

--- prisma/migrations/20251030083521_grievance_resolved_at_closed_at_added/migration.sql
+++ prisma/migrations/20251030083521_grievance_resolved_at_closed_at_added/migration.sql
@@ -1,19 +1,3 @@
--- AlterEnum
-ALTER TYPE "GrievancePriority" ADD VALUE 'CRITICAL';
-
--- AlterEnum
--- This migration adds more than one value to an enum.
--- With PostgreSQL versions 11 and earlier, this is not possible
--- in a single migration. This can be worked around by creating
--- multiple migrations, each migration adding only one value to
--- the enum.
-
-
-ALTER TYPE "GrievanceStatus" ADD VALUE 'IN_PROGRESS';
-ALTER TYPE "GrievanceStatus" ADD VALUE 'REJECTED';
-
--- AlterEnum
-ALTER TYPE "GrievanceType" ADD VALUE 'FINANCIAL';
 
 -- AlterTable
 ALTER TABLE "tbl_grievances" ADD COLUMN     "closedAt" TIMESTAMP(3),
