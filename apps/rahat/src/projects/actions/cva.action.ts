7e8e51fc55ac0ad36c21eb4b2bcda97ce18d1187
bipinparajuli <bipinprjl@gmail.com>
2026-07-06 09:53:09 +0545

    fix:removed usage action

--- apps/rahat/src/projects/actions/cva.action.ts
+++ apps/rahat/src/projects/actions/cva.action.ts
@@ -1,71 +1,45 @@
 // This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 // If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
-import { MS_ACTIONS, ProjectJobs } from "@rahataid/sdk";
-import { ProjectActionFunc } from "@rahataid/sdk/project/project.types";
+import { MS_ACTIONS, ProjectJobs } from '@rahataid/sdk';
+import { ProjectActionFunc } from '@rahataid/sdk/project/project.types';
 
 export const cvaActions: ProjectActionFunc = {
-
   [MS_ACTIONS.CVAProject.REQUEST_CLAIM]: (uuid, payload, sendCommand) =>
-    sendCommand(
-      { cmd: ProjectJobs.REQUEST_CLAIM, uuid },
-      payload,
-      500000
-    ),
+    sendCommand({ cmd: ProjectJobs.REQUEST_CLAIM, uuid }, payload, 500000),
   //campaign start
   [MS_ACTIONS.CVAProject.CREATE_CAMPAIGN]: (uuid, payload, sendCommand) =>
-    sendCommand(
-      { cmd: ProjectJobs.CAMPAIGN.CREATE_CAMPAIGN, uuid },
-      payload
-    ),
+    sendCommand({ cmd: ProjectJobs.CAMPAIGN.CREATE_CAMPAIGN, uuid }, payload),
   [MS_ACTIONS.CVAProject.GET_ALL_CAMPAIGN]: (uuid, payload, sendCommand) =>
-    sendCommand(
-      { cmd: ProjectJobs.CAMPAIGN.LIST_CAMPAIGN, uuid },
-      payload
-    ),
+    sendCommand({ cmd: ProjectJobs.CAMPAIGN.LIST_CAMPAIGN, uuid }, payload),
   [MS_ACTIONS.CVAProject.GET_CAMPAIGN]: (uuid, payload, sendCommand) =>
-    sendCommand(
-      { cmd: ProjectJobs.CAMPAIGN.FINDONE_CAMPAIGN, uuid },
-      payload
-    ),
+    sendCommand({ cmd: ProjectJobs.CAMPAIGN.FINDONE_CAMPAIGN, uuid }, payload),
 
   [MS_ACTIONS.CVAProject.GET_ALL_AUDIENCE]: (uuid, payload, sendCommand) =>
-    sendCommand(
-      { cmd: ProjectJobs.CAMPAIGN.GET_ALL_AUDIENCE, uuid },
-      payload
-    ),
-
+    sendCommand({ cmd: ProjectJobs.CAMPAIGN.GET_ALL_AUDIENCE, uuid }, payload),
 
   [MS_ACTIONS.CVAProject.CREATE_AUDIENCE]: (uuid, payload, sendCommand) =>
-    sendCommand(
-      { cmd: ProjectJobs.CAMPAIGN.CREATE_AUDIENCE, uuid },
-      payload
-    ),
+    sendCommand({ cmd: ProjectJobs.CAMPAIGN.CREATE_AUDIENCE, uuid }, payload),
   [MS_ACTIONS.CVAProject.TRIGGER_CAMPAIGN]: (uuid, payload, sendCommand) =>
-    sendCommand(
-      { cmd: ProjectJobs.CAMPAIGN.TRIGGER_CAMPAIGN, uuid },
-      payload
-    ),
-  [MS_ACTIONS.CVAProject.GET_ALL_COMMUNICATION_LOGS]: (uuid, payload, sendCommand) =>
+    sendCommand({ cmd: ProjectJobs.CAMPAIGN.TRIGGER_CAMPAIGN, uuid }, payload),
+  [MS_ACTIONS.CVAProject.GET_ALL_COMMUNICATION_LOGS]: (
+    uuid,
+    payload,
+    sendCommand
+  ) =>
     sendCommand(
       { cmd: ProjectJobs.CAMPAIGN.GET_ALL_COMMUNICATION_LOGS, uuid },
       payload
     ),
-  [MS_ACTIONS.CVAProject.GET_ALL_COMMUNICATION_STATS]: (uuid, payload, sendCommand) =>
+  [MS_ACTIONS.CVAProject.GET_ALL_COMMUNICATION_STATS]: (
+    uuid,
+    payload,
+    sendCommand
+  ) =>
     sendCommand(
       { cmd: ProjectJobs.CAMPAIGN.GET_ALL_COMMUNICATION_STATS, uuid },
       payload
     ),
   [MS_ACTIONS.CVAProject.GET_ALL_TRANSPORT]: (uuid, payload, sendCommand) =>
-    sendCommand(
-      { cmd: ProjectJobs.CAMPAIGN.GET_ALL_TRANSPORT, uuid },
-      payload
-    ),
+    sendCommand({ cmd: ProjectJobs.CAMPAIGN.GET_ALL_TRANSPORT, uuid }, payload),
   //campaign end
-
-  [MS_ACTIONS.ELPROJECT.CRM.GET_COMMS_USAGE]: (uuid, payload, sendCommand) =>
-    sendCommand({ cmd: ProjectJobs.CAMPAIGN.GET_COMMS_USAGE, uuid }, payload),
-
-  [MS_ACTIONS.ELPROJECT.CRM.GET_COMMS_CREDITS]: (uuid, payload, sendCommand) =>
-    sendCommand({ cmd: ProjectJobs.CAMPAIGN.GET_COMMS_CREDITS, uuid }, payload),
-
-}
+};
