590260c53a53436d48cd49a74740983febfd7688
amrit-rumsan <amrit@rumsan.net>
2026-07-15 23:29:06 +0545

    fix: add username on vendor loginByPassword response

--- apps/rahat/src/vendors/vendors.service.ts
+++ apps/rahat/src/vendors/vendors.service.ts
@@ -937,6 +937,7 @@ export class VendorsService {
         id: user.id,
         uuid: user.uuid,
         name: user.name,
+        username: user.username,
         email: user.email,
         phone: user.phone,
         wallet: user.wallet,
