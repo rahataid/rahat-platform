bb8508b4a20a9fcd181fb4513165a7a98b34b71e
amrit-rumsan <amrit@rumsan.net>
2026-07-15 23:25:59 +0545

    feat: add api endpoint for vendor password change

--- apps/rahat/src/vendors/vendors.service.ts
+++ apps/rahat/src/vendors/vendors.service.ts
@@ -15,12 +15,17 @@ import {
   VendorRegisterDto,
 } from '@rahataid/extensions';
 import { ProjectContants, UserRoles, VendorJobs } from '@rahataid/sdk';
-import { OtpDto, OtpLoginDto, PasswordLoginDto } from '@rumsan/extensions/dtos';
+import {
+  ChangePasswordDto,
+  OtpDto,
+  OtpLoginDto,
+  PasswordLoginDto,
+} from '@rumsan/extensions/dtos';
 import { PaginatorTypes, PrismaService, paginator } from '@rumsan/prisma';
 import { CONSTANTS } from '@rumsan/sdk/constants/index';
 import { Service } from '@rumsan/sdk/enums';
 import { Request } from '@rumsan/sdk/types';
-import { AuthsService, SignupsService } from '@rumsan/user';
+import { AuthsService, CurrentUserInterface, SignupsService } from '@rumsan/user';
 import { decryptChallenge } from '@rumsan/user/lib/utils/challenge.utils';
 import { getSecret } from '@rumsan/user/lib/utils/config.utils';
 import { getServiceTypeByAddress } from '@rumsan/user/lib/utils/service.utils';
@@ -939,6 +944,21 @@ export class VendorsService {
       wallet,
     };
   }
+  async changeVendorPassword(user: CurrentUserInterface, dto: ChangePasswordDto) {
+    const isVendor = user.roles?.includes(UserRoles.VENDOR);
+    if (!isVendor) {
+      throw new ForbiddenException('User is not a vendor');
+    }
+
+    return this.authService.updatePassword(
+      user.id,
+      dto.oldPassword,
+      dto.newPassword,
+      dto.confirmPassword,
+      dto.service
+    );
+  }
+
   async create(payload: any) {
     const { projectId, vendors: vendorsList } = payload;
 
