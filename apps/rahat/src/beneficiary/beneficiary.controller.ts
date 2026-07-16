f558daba485b0cf9f01ae3f27aa976e9b4e6f395
Akash-rumsan <akash.rumsan@gmail.com>
2026-07-16 18:05:01 +0545

    test

--- apps/rahat/src/beneficiary/beneficiary.controller.ts
+++ apps/rahat/src/beneficiary/beneficiary.controller.ts
@@ -141,6 +141,7 @@ export class BeneficiaryController {
   async getStats() {
     const commsClient = await this.commsService.getClient();
     const commsStats = await commsClient.broadcast.getReport({})
+    console.log("commsStats")
     const benefStats = await firstValueFrom(this.client.send({ cmd: BeneficiaryJobs.STATS }, {}));
     return { data: { commsStats: commsStats.data, benefStats: benefStats } };
   }
